506438052e72b4df81cbbcd50c3174cf0ecd7bde
Quentin <qmialon@groupeirfaformation.fr>
2024-11-29 10:18:21 +0100

    cors adjusting for dev environments

--- routes/api.php
+++ routes/api.php
@@ -11,29 +11,34 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['post'],base_path('routes/api/auth.php'));
 
-//POI routes
-Route::post('/batiments/import', [BatimentController::class, 'import']);
-Route::post('/batiments', [BatimentController::class, 'store']);
-Route::delete('/batiments/{id}', [BatimentController::class, 'destroy']);
-Route::get('/batiments', [BatimentController::class, 'index']);
-Route::get('/batiments/{id}', [BatimentController::class, 'show']);
+Route::middleware(['api'])->group(function () {
+    Route::group(['post'],base_path('routes/api/auth.php'));
 
-//visited routes
-Route::post('/visited',[VisitedController::class,'store']);
-Route::get('/visited',[VisitedController::class,'ReadAllFromUser']);
-Route::get('/visited/count',[VisitedController::class,'countVisit']);
+    //POI routes
+    Route::post('/batiments/import', [BatimentController::class, 'import']);
+    Route::post('/batiments', [BatimentController::class, 'store']);
+    Route::delete('/batiments/{id}', [BatimentController::class, 'destroy']);
+    Route::get('/batiments', [BatimentController::class, 'index']);
+    Route::get('/batiments/{id}', [BatimentController::class, 'show']);
+
+    //visited routes
+    Route::post('/visited',[VisitedController::class,'store']);
+    Route::get('/visited',[VisitedController::class,'ReadAllFromUser']);
+    Route::get('/visited/count',[VisitedController::class,'countVisit']);
 
 
-//tour routes
-Route::post('/tours', [TourController::class, 'store']);
-Route::get('/tours', [TourController::class, 'index']);
-Route::get('/tours/details', [TourController::class, 'show']);
+    //tour routes
+    Route::post('/tours', [TourController::class, 'store']);
+    Route::get('/tours', [TourController::class, 'index']);
+    Route::get('/tours/details', [TourController::class, 'show']);
 
-//trip routes
-Route::post('/trips/start', [TripController::class,'startTrip']);
-Route::post('/trips/pitstop/validate', [TripController::class,'validatePitstop']);
-Route::post('/trips/complete', [TripController::class,'completeTrip']);
+    //trip routes
+    Route::post('/trips/start', [TripController::class,'startTrip']);
+    Route::post('/trips/pitstop/validate', [TripController::class,'validatePitstop']);
+    Route::post('/trips/complete', [TripController::class,'completeTrip']);
+
+
+});
 
 
